382d854a9a977876cc75154d18e49e05f8c6e985
Harsh Kedia <hkedia@hey.com>
2025-04-26 15:43:47 +0530

    integer reversal

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // Convert the number to a string, reverse it, and convert it back to a number
+  const reversed = parseInt(n.toString().split('').reverse().join(''));
+
+  // If the original number is negative, return the negative of the reversed number
+  return n < 0 ? -reversed : reversed;
+}
 
 module.exports = reverseInt;
