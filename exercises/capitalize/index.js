3a45ca20edd1166c81aa33750d93a0bb880fc786
Harsh Kedia <hkedia@hey.com>
2025-04-26 18:20:53 +0530

    capitalized string

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,17 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // Split the string into words
+  const words = str.split(' ');
+
+  // Capitalize the first letter of each word
+  const capitalizedWords = words.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+
+  // Join the capitalized words back into a string
+  return capitalizedWords.join(' ');
+}
 
 module.exports = capitalize;
