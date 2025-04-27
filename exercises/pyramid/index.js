66750a6c8c2d990195d7c336ed98cab90be8bbce
Harsh Kedia <hkedia@hey.com>
2025-04-27 12:36:39 +0530

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,13 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const maxWidth = n * 2 - 1;
+  for (let i = 0; i < n; i++) {
+    const numHashes = i * 2 + 1;
+    const numSpaces = (maxWidth - numHashes) / 2;
+    console.log(' '.repeat(numSpaces) + '#'.repeat(numHashes) + ' '.repeat(numSpaces));
+  }
+}
 
 module.exports = pyramid;
