23847c0484744545588c656c1dfa6965b2ad00b0
Harsh Kedia <hkedia@hey.com>
2025-04-30 09:56:08 +0530

    events

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,40 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    // Check if the eventName already exists
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    // Add the callback to the eventName
+    this.events[eventName].push(callback);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    // Check if the eventName exists
+    if (this.events[eventName]) {
+      // Call each callback associated with the eventName
+      this.events[eventName].forEach((callback) => {
+        callback();
+      });
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    // Check if the eventName exists
+    if (this.events[eventName]) {
+      // Remove the eventName from the events object
+      delete this.events[eventName];
+    }
+  }
 }
 
 module.exports = Events;
