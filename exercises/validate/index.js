b60f71732e8868b13e683399c260a8dbe3b80451
Harsh Kedia <hkedia@hey.com>
2025-04-30 07:52:44 +0530

    validating a bst

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,19 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  // If the node is null, return true
+  if (!node) {
+    return true;
+  }
+
+  // Check if the node's value is within the min and max range
+  if ((min !== null && node.data <= min) || (max !== null && node.data >= max)) {
+    return false;
+  }
+
+  // Recursively check the left and right subtrees
+  return validate(node.left, min, node.data) && validate(node.right, node.data, max);
+}
 
 module.exports = validate;
